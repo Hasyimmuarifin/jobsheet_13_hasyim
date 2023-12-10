1b8aab4b9a8492e2e2137a69f4e1f1f5c77da639
Hasyimmuarifin <hasyimviic@gmail.com>
2023-12-10 17:48:27 +0700

    commit kedua

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='hasyim12',
+  version='0.1.0',
+  author='Hasyim',
+  author_email='hasyimviic@gmail.com',
+  description='Ini adalah Contoh Package yang saya Buat',
+  packages=find_packages(),
+  classifiers=[
+    'Programming Language :: Python :: 3',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+  ],
+  python_requires='>=3.6',
+)
